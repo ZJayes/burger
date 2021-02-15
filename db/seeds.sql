7aea585306abb606bbe5711c06fa9acda832af67
Zachary Jayes <zacjayes@gmail.com>
2021-02-14 22:14:38 -0600

    db setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('BigMac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', DEFAULT);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', true)
