7cf95df156c4530728e1878aac9c5a438144d025
Zachary Jayes <zacjayes@gmail.com>
2021-02-16 17:20:41 -0600

    finished controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,6 +5,7 @@ const router = express.Router();
 //creating the routes
 
 //get route
+
 router.get("/", function(req,res) {
  burger.all(function(data) {
      var hbsObject = {
@@ -15,8 +16,39 @@ router.get("/", function(req,res) {
  })
 });
 
+//post route
 
+router.post("/api/burger", function(req, res) {
+    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
 
+  //put route
+  //used to update the route
+
+  router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.updateOne(
+      {
+        devoured: req.body.devoured
+      },
+      condition,
+      function (result) {
+        if (result.changedRows === 0) {
+  
+          return res.status(404).end();
+        }
+        res.status(200).end();
+  
+      }
+    );
+  });
+  
 
 
 
