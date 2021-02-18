f4bbeb4581688e00ae7194194388565295adc73f
Zachary Jayes <zacjayes@gmail.com>
2021-02-17 20:51:50 -0600

    deleting broken code, tutor friday

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,5 @@
 const connection = require('./connection')
 
-// helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
 
 function printQuestionMarks(num) {
     var arr = [];
@@ -12,35 +11,24 @@ function printQuestionMarks(num) {
     return arr.toString();
   }
 
-  // Helper function to convert object key/value pairs to SQL syntax
-
-function objToSql(ob) {
- var arr = [];
-
-// loop through the keys and push the key/value as a string int arr
-
-for (var key in ob) {
-    var value = ob[key];
-
-// check to skip hidden properties
-
-if (Object.hasOwnProperty.call(ob, key)) {
-   
-    if (typeof value === "string" && value.indexOf(" ") >= 0) {
-      value = "'" + value + "'";
+  
+  function objToSql(ob) {
+    var arr = [];
+  
+    for (var key in ob) {
+      var value = ob[key];
+      if (Object.hasOwnProperty.call(ob, key)) {
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
     }
-   
-    arr.push(key + "=" + value);
-    }
-
+  
+    return arr.toString();
   }
-
-  return arr.toString();
-}
-
-// Object for all our SQL statement functions.
-
-var orm = {
+  
+  var orm = {
     all: function(tableInput, cb) {
       var queryString = "SELECT * FROM " + tableInput + ";";
       connection.query(queryString, function(err, result) {
@@ -70,31 +58,28 @@ var orm = {
         cb(result);
       });
     },
-
+    
     update: function(table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
+      var queryString = "UPDATE " + table;
+  
+      queryString += " SET ";
+      queryString += objColVals;
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
     
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE ";
-        queryString += condition;
+  
+
     
-        console.log(queryString);
-        connection.query(queryString, function(err, result) {
-          if (err) {
-            throw err;
-          }
-    
-          cb(result);
-        });
-      }
-    };
-
-
-
-
-
-
-
-
-module.exports = orm;
+  };
+  
+  module.exports = orm;
